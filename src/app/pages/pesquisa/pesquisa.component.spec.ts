e63cf651f2a0b2337a7bf103ee6473cebac16825
jooemanoel <jooemanoel@gmail.com>
2024-12-02 23:19:26 -0300

    ok

--- src/app/pages/pesquisa/pesquisa.component.spec.ts
+++ src/app/pages/pesquisa/pesquisa.component.spec.ts
@@ -5,24 +5,34 @@ import { of, Subject, throwError } from 'rxjs';
 import { ClienteService } from 'src/app/services/cliente.service';
 import { Cliente } from 'src/app/shared/models/interfaces/cliente';
 import { PesquisaComponent } from './pesquisa.component';
+import { ControleService } from 'src/app/services/controle.service';
 
 describe('PesquisaComponent', () => {
   let component: PesquisaComponent;
   let fixture: ComponentFixture<PesquisaComponent>;
+  let controleService: ControleService;
   const clienteAlterado = new Subject<Cliente>();
   const serviceMock = jasmine.createSpyObj('ClienteService', [], {
     getClienteAtual: () => of({
       nome: 'Joao',
       codigo: 123
     }),
-    detectarClienteAlterado: () => clienteAlterado.asObservable()
+    detectarClienteAlterado: () => clienteAlterado.asObservable(),
+    chamarProximoCliente() {
+      clienteAlterado.next({
+        nome: 'Maria',
+        codigo: 456
+      });
+    }
   });
   beforeEach(() => {
+    controleService = new ControleService();
     TestBed.configureTestingModule({
       schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
       declarations: [PesquisaComponent],
       providers: [
-        { provide: ClienteService, useValue: serviceMock }
+        { provide: ClienteService, useValue: serviceMock },
+        { provide: ControleService, useValue: controleService }
       ]
     });
   });
@@ -44,5 +54,13 @@ describe('PesquisaComponent', () => {
     spyOn(component, 'mostrarErro').and.callThrough();
     fixture.detectChanges();
     expect(component.mostrarErro).not.toHaveBeenCalled();
+  });
+  it('`Cliente alterado`', () => {
+    fixture = TestBed.createComponent(PesquisaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    component.buscar();
+    fixture.detectChanges();
+    expect(controleService.cliente.nome).toEqual('Maria');
   });
 });
